2d9a0144837a8df24fb8a0dd3fe97fbcec1b4375
Tobie Warburton <tobie.warburton@gmail.com>
2014-02-13 12:25:03 +0000

    Errors caught from the API are now raised

--- lib/trakio.rb
+++ lib/trakio.rb
@@ -7,8 +7,17 @@ class Trakio
 
   class Exceptions
     class Uninitiated < RuntimeError; end
-    class InvalidToken < RuntimeError; end
     class NoDistinctIdForDefaultInstance < RuntimeError; end
+    class DataObjectInvalidJson < RuntimeError; end
+    class DataObjectInvalidBase64 < RuntimeError; end
+    class DataObjectInvalidType < RuntimeError; end
+    class InvalidToken < RuntimeError; end
+    class MissingParameter < RuntimeError; end
+    class RouteNotFound < RuntimeError; end
+    class PropertiesObjectInvalid < RuntimeError; end
+    class RequestInvalidJson < RuntimeError; end
+    class RevenuePropertyInvalid < RuntimeError; end
+    class InternalServiceError < RuntimeError; end
   end
 
   class << self
@@ -157,7 +166,47 @@ class Trakio
     url = "#{protocol}://#{@host}/#{endpoint}"
     data = { token: @api_token, data: params }.to_json
     resp = RestClient.post url, data, :content_type => :json, :accept => :json
-    JSON.parse(resp.body, :symbolize_names => true)
+    result = JSON.parse(resp.body, :symbolize_names => true)
+    return result if result[:status] == 'success'
+
+    # status must be error
+    # here we will raise the required exception as in the API
+    exception = constantize(result[:exception].sub! 'TrakioAPI::', 'Trakio::') # name of the class
+    message = result[:message] # extra information for the exception
+    raise exception.new(message)
+  end
+
+  protected
+
+  def constantize(camel_cased_word) # Taken from ActiveSupport
+    names = camel_cased_word.split('::')
+
+    # Trigger a builtin NameError exception including the ill-formed constant in the message.
+    Object.const_get(camel_cased_word) if names.empty?
+
+    # Remove the first blank element in case of '::ClassName' notation.
+    names.shift if names.size > 1 && names.first.empty?
+
+    names.inject(Object) do |constant, name|
+      if constant == Object
+        constant.const_get(name)
+      else
+        candidate = constant.const_get(name)
+        next candidate if constant.const_defined?(name, false)
+        next candidate unless Object.const_defined?(name)
+
+        # Go down the ancestors to check it it's owned
+        # directly before we reach Object or the end of ancestors.
+        constant = constant.ancestors.inject do |const, ancestor|
+          break const    if ancestor == Object
+          break ancestor if ancestor.const_defined?(name, false)
+          const
+        end
+
+        # owner is in Object, so raise
+        constant.const_get(name, false)
+      end
+    end
   end
 
 end
---
+++ spec/trakio/exception_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe Trakio do
+
+  subject { Trakio }
+
+  after {
+    Trakio.default_instance = nil
+  }
+
+  describe '.track' do
+
+    context "when an error is returned by the API" do
+      it "raises an exception" do
+        stub = stub_request(:post, "https://api.trak.io/v1/track").
+          with(:body => {
+            token: 'my_api_token',
+            data: {
+              distinct_id: 'user@example.com',
+              event: 'my-event'
+            }
+          }).to_return(:body => {
+            status: 'error',
+            code: 401,
+            exception: "TrakioAPI::Exceptions::InvalidToken",
+            message: "Missing or invalid API token.",
+            details: "You must provide a valid API token, see http://docs.trak.io/authentication.html."
+          }.to_json)
+
+        trakio = Trakio.new 'my_api_token'
+        expect {
+          trakio.track distinct_id: 'user@example.com', event: 'my-event'
+        }.to raise_error Trakio::Exceptions::InvalidToken
+
+        stub.should have_been_requested
+
+      end
+    end
+
+    context "when no error is returned by the API" do
+      it "returns the result" do
+        stub = stub_request(:post, "https://api.trak.io/v1/track").
+          with(:body => {
+            token: 'my_api_token',
+            data: {
+              distinct_id: 'user@example.com',
+              event: 'my-event'
+            }
+          }).to_return(:body => {
+            status: 'success',
+            trak_id: '1234567890'
+          }.to_json)
+
+        trakio = Trakio.new 'my_api_token'
+        resp = trakio.track distinct_id: 'user@example.com', event: 'my-event'
+
+        expect(resp[:status]).to eql 'success'
+        expect(resp[:trak_id]).to eql '1234567890'
+
+        stub.should have_been_requested
+      end
+    end
+
+  end # end .track
+end
